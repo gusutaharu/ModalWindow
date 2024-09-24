8adfc0d4af10155718e46a4fd38794ab054bed23
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-09-24 10:04:05 +0900

    lesson06

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict';
+{
+  const open = document.querySelector('open');
+  const close = document.querySelector('close');
+  const modal = document.querySelector('modal');
+  const mask = document.querySelector('mask');
+
+}
